d503c1881cc66b05f024bb93be31cae7e4d97494
Zinath22 <Zinath22@users.noreply.github.com>
2023-09-30 22:15:59 +0600

    create user added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
+// import { useContext } from "react";
+// import { AuthContext } from "../providers/AuthProvider";
 
 
 const Home = () => {
+    // const authInfo = useContext(AuthContext);
+    // console.log(authInfo);
+
     return (
         <div>
-            <h2>This is home</h2>
+            <h2>This is home for: </h2>
         </div>
     );
 };
