8350db99a62b8797f051cfcfd8a5f269f2861a31
Zinath22 <Zinath22@users.noreply.github.com>
2023-09-30 20:42:13 +0600

    register page added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,10 +10,10 @@ const Login = () => {
     }
     return (
         <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
+        <div className="hero-content flex-col ">
+          <div className="text-center ">
             <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
@@ -38,7 +38,7 @@ const Login = () => {
               </div>
             </form>
 
-            <p>New to Auth? Please <Link to="/register">
+            <p>New? Please  <Link to="/register">
             <button className="btn btn-link">Register</button>
             </Link> </p>
             </div>
